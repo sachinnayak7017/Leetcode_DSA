ff9f8d0eaddbc8ac08a33a00ab3a8679891cdc3b
sachin kumar <156423031+sachinnayak7017@users.noreply.github.com>
2026-08-22 23:59:19 +0530

    Time: 45 ms (57.59%), Space: 91 MB (39.1%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -1,51 +1,18 @@
+import java.util.Arrays;
+
 class Solution {
     public int maximumGap(int[] nums) {
 
-        if (nums.length < 2)
+        if (nums.length < 2) {
             return 0;
-
-        int n = nums.length;
-
-        int min = nums[0];
-        int max = nums[0];
-
-        for (int num : nums) {
-            min = Math.min(min, num);
-            max = Math.max(max, num);
         }
 
-        if (min == max)
-            return 0;
-
-        int bucketSize = (int) Math.ceil((double) (max - min) / (n - 1));
-        int bucketCount = (max - min) / bucketSize + 1;
-
-        int[] minBucket = new int[bucketCount];
-        int[] maxBucket = new int[bucketCount];
-        boolean[] bucketUsed = new boolean[bucketCount];
-
-        Arrays.fill(minBucket, Integer.MAX_VALUE);
-        Arrays.fill(maxBucket, Integer.MIN_VALUE);
-
-        for (int num : nums) {
-
-            int index = (num - min) / bucketSize;
-
-            minBucket[index] = Math.min(minBucket[index], num);
-            maxBucket[index] = Math.max(maxBucket[index], num);
-            bucketUsed[index] = true;
-        }
+        Arrays.sort(nums);
 
         int maxGap = 0;
-        int prevMax = maxBucket[0];
 
-        for (int i = 1; i < bucketCount; i++) {
-
-            if (!bucketUsed[i])
-                continue;
-
-            maxGap = Math.max(maxGap, minBucket[i] - prevMax);
-            prevMax = maxBucket[i];
+        for (int i = 1; i < nums.length; i++) {
+            maxGap = Math.max(maxGap, nums[i] - nums[i - 1]);
         }
 
         return maxGap;
