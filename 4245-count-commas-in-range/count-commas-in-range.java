cbc32db0de7d7f08232a892454aa06d11f710324
sachin kumar <156423031+sachinnayak7017@users.noreply.github.com>
2026-09-08 21:07:10 +0530

    Time: 1 ms (83.65%) | Memory: 42.4 MB (72.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,5 +1,21 @@
 class Solution {
     public int countCommas(int n) {
-        return (n >= 1000) ? (n - 999) : 0;
+        int ans = 0;
+
+        long start = 1000; // first number having 1 comma
+        int commas = 1;
+
+        while (start <= n) {
+            long end = start * 1000 - 1;
+
+            long countNumbers = Math.min((long)n, end) - start + 1;
+
+            ans += countNumbers * commas;
+
+            start *= 1000;
+            commas++;
+        }
+
+        return ans;
     }
 }
